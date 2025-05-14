c1483dabe61c9f520f6a5ccb8b2401bea458f559
Brian-Rotich20 <ralphrotich@gmail.com>
2025-05-14 22:22:34 +0300

    Initial commit

---
+++ blogapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title 
---
+++ blogapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Blog
+
+# Create your views here.
+def index(request):
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs} 
+    return render(request, 'index.html', context)
+
+def detail(request, pk):
+    blog = Blog.objects.get(id=pk)
+    context = {"blog": blog}
+    return render(request, 'detail.html', context)
